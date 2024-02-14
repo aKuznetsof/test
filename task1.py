e23b704f1acc5c44ccf6b10306b24a53e77f1d07
aKuznetsof <160076237+aKuznetsof@users.noreply.github.com>
2024-02-15 02:51:27 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,17 @@
+num = int(input())
+max_sum = 0
+num_max_sum = 0
+while num != 0:
+    temp_num = num
+    sum = 0
+    while num > 0:
+        sum += num % 10
+        num //= 10
+    if sum > max_sum:
+        max_sum = sum
+        num_max_sum = temp_num
+    num = int(input())
+print('Число',num_max_sum,'имеет максимальную сумму цифр:', max_sum)
+
+print('\nНажмите Enter для выхода.')
+exit = input()
